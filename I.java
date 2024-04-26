380713c0acf496e32d054bd724db79e127db3aa4
nik <s367231@itmo.ru>
2024-04-26 08:43:01 +0300

    r0

---
+++ I.java
@@ -0,0 +1,14 @@
+public class I {
+
+    private long c = 4321;
+
+    private String h = "init";
+
+    public float ff() {
+        return 0;
+    }
+
+    public void aa() {
+        System.out.println("void aa");
+    }
+}
