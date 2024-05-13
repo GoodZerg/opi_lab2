86e44eb1313eee7453e8489efdde296240110e05
nik2 <s367056@itmo.ru>
2024-05-13 16:14:25 +0300

    r14

--- I.java
+++ I.java
@@ -1,4 +1,4 @@
-public class I {
+public class I extends null {
 
     private long c = 4321;
 
@@ -18,5 +18,37 @@ public class I {
 
     public Object rr() {
         return null;
+    }
+
+    public int hh() {
+        return new java.util.Random(10).nextInt(10);
+    }
+
+    public long ac() {
+        return 333;
+    }
+
+    public java.util.List<String> jj() {
+        return new java.util.LinkedList<String>();
+    }
+
+    public int ae() {
+        return java.lang.Math.abs(-7);
+    }
+
+    public java.util.Set<Integer> ll() {
+        return new java.util.LinkedList<Integer>;
+    }
+
+    public java.util.Random mm() {
+        return new java.util.Random();
+    }
+
+    public int[] ii() {
+        return new int[]{0, 1, 2, 3, 4};
+    }
+
+    public long dd() {
+        return 100500;
     }
 }
